5e5f6f7dcc4e92e4b905de8d7b6fd5691002c1f2
Frank Stelzer <frank.stelzer@chefkoch.de>
2017-04-02 01:01:13 +0200

    prepared entity wrappers for extracted recipe model

--- apps/monolith/src/AppBundle/Repository/RecipeRepository.php
+++ apps/monolith/src/AppBundle/Repository/RecipeRepository.php
@@ -42,4 +42,42 @@ class RecipeRepository
 
         return json_decode($response, true);
     }
+
+    /*
+    public function findById($id)
+    {
+        $data = $this->curl('http://recipe/api/recipes/'.$id);
+        if (!$data) {
+            throw new \InvalidArgumentException('recipe not found');
+        }
+
+        return new Recipe($data);
+    }
+
+    public function findLatest()
+    {
+        $entries = $this->curl('http://recipe/api/recipes');
+        $list = [];
+        foreach ($entries as $data) {
+            $list[] = new Recipe($data);
+        }
+
+        return $list;
+    }
+    */
+
+    private function curl($url)
+    {
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_HEADER, 0);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+        $response = curl_exec($ch);
+
+        curl_close($ch);
+
+        return json_decode($response, true);
+    }
+
 }
