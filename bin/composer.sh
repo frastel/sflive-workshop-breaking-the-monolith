681c0a067c04cc7742db204366f889271513cc25
Frank Stelzer <frank.stelzer@chefkoch.de>
2017-04-01 22:49:58 +0200

    prepared composer script for other services

--- bin/composer.sh
+++ bin/composer.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
 cmd=${1:-install}
+app=${2:-monolith}
 
+shift
 shift
 
 what=$@
 
 docker run --rm -ti \
-    --volume $(pwd)/apps/monolith:/app \
+    --volume $(pwd)/apps/${app}:/app \
     --volume $(pwd)/.composer:/composer \
     composer:1.3 $cmd $what
