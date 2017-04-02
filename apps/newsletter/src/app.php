6931dcba6341a944f67333d38d90065c5a75157b
Frank Stelzer <frank.stelzer@chefkoch.de>
2017-04-02 03:06:31 +0200

    moved newsletter box to dedicatedd app and cleaned monolith

--- apps/newsletter/src/app.php
+++ apps/newsletter/src/app.php
@@ -21,6 +21,19 @@ $app->get('/newsletter/ping', function() use($app) {
     return '[Newsletter] Pong!';
 });
 
-// place additional routes here!!!
+$app->get('/newsletter/box', function() use($app) {
+    return $app['twig']->render('box.html.twig', []);
+});
+
+$app->get('/newsletter/bootstrap', function() use($app) {
+    return $app['twig']->render('bootstrap.html.twig', []);
+});
+
+$app->post('/newsletter', function() use($app) {
+
+    // do some persistence logic here ...
+
+    return $app['twig']->render('thankyou.html.twig', []);
+});
 
 return $app;
